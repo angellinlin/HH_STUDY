1b7669209e5ef1078bc8d0656c9baa9443d46e67
ljl <1274042829@qq.com>
2020-07-03 12:59:59 +0800

    first commit, build server Framework

---
+++ HR_SYS_SERVER/src/main/java/com/spdb/hrsys/HrsysApplication.java
@@ -0,0 +1,13 @@
+package com.spdb.hrsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsysApplication.class, args);
+	}
+
+}

a21f7c813e07fcf3bb153c0e1be08e90d4dd1d74
ljl <1274042829@qq.com>
2020-07-08 23:33:08 +0800

    整合Swagger UI

--- HR_SYS_SERVER/src/main/java/com/spdb/hrsys/controller/TestController.java
+++ HR_SYS_SERVER/src/main/java/com/spdb/hrsys/controller/TestController.java
@@ -3,6 +3,8 @@ package com.spdb.hrsys.controller;
 import com.spdb.hrsys.mbg.model.Test;
 import com.spdb.hrsys.service.MemberService;
 import com.spdb.hrsys.service.TestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+@Api(tags = "测试Swagger")
 @RestController
 @RequestMapping("/hrsys")
 public class TestController {
@@ -28,7 +29,7 @@ public class TestController {
 
     @Autowired
     private MemberService memberService;
-
+    @ApiOperation("测试数据库连接、MyBatis")
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public ResponseEntity<?> test(){
         List<Test> list = new ArrayList<>();
@@ -40,16 +41,17 @@ public class TestController {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-
         return ResponseEntity.ok(list);
     }
 
+    @ApiOperation("模拟获取手机验证码，测试redis")
     @RequestMapping(method = RequestMethod.GET, value = "/getAuthCode")
     public ResponseEntity<?> getAuthCode(@RequestParam String phone){
         String authCode =  memberService.generateAuthCode(phone);
         return ResponseEntity.ok(authCode);
     }
 
+    @ApiOperation("模拟验证码是否正确，测试redis")
     @RequestMapping(method = RequestMethod.POST, value = "/verifyAuthCode")
     public ResponseEntity<?> verifyAuthCode(@RequestParam String phone, @RequestParam String authCode){
         boolean isRightCode =  memberService.verifyAuthCode(phone, authCode);
--- HR_SYS_SERVER/src/main/java/com/spdb/hrsys/mbg/CommentGenerator.java
+++ HR_SYS_SERVER/src/main/java/com/spdb/hrsys/mbg/CommentGenerator.java
@@ -1,6 +1,56 @@
 package com.spdb.hrsys.mbg;
 
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
 
 public class  CommentGenerator extends DefaultCommentGenerator {
+    private boolean addRemarkComments = false;
+    private static final String EMPTY_SUFFIX = "Example";
+    private static final String API_MODEL_PROPERTY_FULL_CLASS_NAME = "io.swagger.annotations.ApiModelProperty";
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+    /**
+     * 给字段加注释
+     */
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)){
+            if (remarks.contains("\""))
+                remarks.replace("\"", "'");
+            field.addJavaDocLine("@ApiModelProperty(value=\"" + remarks + "\")");
+        }
+    }
+
+    /**
+     * 给model添加注释
+     */
+    private void addModelJavaDoc(Field field, String remarks){
+        field.addJavaDocLine("/**");
+        String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+        for (String remarkLine: remarkLines){
+            field.addJavaDocLine("*"+ remarkLine);
+        }
+        addJavadocTag(field, false);
+        field.addJavaDocLine("*/");
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        super.addJavaFileComment(compilationUnit);
+        if (!compilationUnit.getType().getFullyQualifiedName().contains(EMPTY_SUFFIX))
+            compilationUnit.addImportedType(new FullyQualifiedJavaType(API_MODEL_PROPERTY_FULL_CLASS_NAME));
+    }
 }

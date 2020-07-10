ee02331f94cbba2ff20ab307a792fb85ba131d1f
weihongyu <497613296@qq.com>
2020-07-10 22:13:52 +0800

    hr_sys_01 控制台版本

---
+++ hr_sys_01/src/main/java/com/weihongyu/HR_sys.java
@@ -0,0 +1,16 @@
+package com.weihongyu;
+
+import com.weihongyu.service.ResumeOperater;
+
+/**
+ * @Auther: weihongyu
+ * @Date: 2020/7/10 20:17
+ * @Description:
+ */
+public class HR_sys {
+
+    public static void main(String[] args) {
+        ResumeOperater ro = new ResumeOperater();
+        ro.run();
+    }
+}

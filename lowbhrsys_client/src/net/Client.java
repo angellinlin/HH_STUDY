8fdc3181578ca30a865153f46d91f723eeb39a51
afteryou <545597348@qq.com>
2020-07-24 12:18:37 +0800

    lichangshu 2rd submit

--- lowbhrsys_client/src/net/Client.java
+++ lowbhrsys_client/src/net/Client.java
@@ -19,6 +19,7 @@ public class Client {
     Socket tcpConnection;
 
     public Client() {
+        //怪了
         try {
             tcpConnection = new Socket("127.0.0.1", 1235);
             System.out.print("连接上服务器了");
